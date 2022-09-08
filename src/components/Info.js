1dc9fa10ecf27113bfff169aebe5d65457961f0f
miami-coder <defenderxz229@gmail.com>
2022-09-08 20:55:06 +0300

    login, control

---
+++ src/components/Info.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from '../styles/Control.module.scss'
+
+export default function Info() {
+  return (
+    <div>
+      <button>+</button>
+    </div>
+  )
+}
--- src/pages/control.js
+++ src/pages/control.js
@@ -1,10 +1,14 @@
+import { style } from '@mui/system'
 import React from 'react'
 import ClippedDrawer from '../components/ClippedDrawer'
+import Info from '../components/Info'
+import styles from '../styles/Control.module.scss'
 
 export default function control() {
   return (
-    <div>
+    <div className={styles.info}>
         <ClippedDrawer />
+        <Info />
     </div>
   )
 }
